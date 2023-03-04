86854b05ceef87da9041bce232ea76c4273b6d8c
Arash-Seifi <arashseifi81@gmail.com>
2023-03-04 23:01:12 +0330

    PLZ-review-me

--- resources/views/industry.blade.php
+++ resources/views/industry.blade.php
@@ -4,12 +4,44 @@
 @include('components.mainHeroBox')
 @include('components.industryItems')
 
-<div class="container-fluid industryLinkToOtherContainer">
-    <div class="row">
-        <div class="col-12 col-md-4">arash</div>
-        <div class="col-12 col-md-4">arash</div>
-        <div class="col-12 col-md-4">arash</div>
+<div class="container-fluid industryBigImageContainer">
+    <div class="container">
+        <img src="/images/WorkWithUs.jpg" alt="">
+        <div class="row">
+            <section>
+                <h1 class="mb-3">ArashSeifi</h1>
+                <h3>SALAM manam Arash , fekr konam esmam ro bedoni, hme ja hast</h3>
+            </section>
+        </div>
     </div>
 </div>
 
+<div class="container-fluid industryLinkToOtherContainer">
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <h1>Shahin</h1>
+            <p>In shahin kos khol ke 1 INPUT nemitone dorost kone , miyad be man harf mizane
+                1 roz rafte JER dadi khodet ro, code ro ANGOS kardi , natonesti
+                bad miyay migi khodam hmaro mizanam< left bede
+            </p>
+            <button>ARASH</button>
+        </div>
+        <div class="col-12 col-md-4">
+            <h1>Shahin</h1>
+            <p>dadash to kheili ghavi hasti , ma ke chizi nagoftim ke to dargiri mikoni,
+                age dava dari ke bahsesh jodas , zang bezan ba bache ha berizim onja, mashhad ro mesl babol, tasarof konim
+            </p>
+        </div>
+        <div class="col-12 col-md-4">
+            <h1>Shahin</h1>
+            <p>In shahin kos khol ke 1 INPUT nemitone dorost kone , miyad be man harf mizane
+                1 roz rafte JER dadi khodet ro, code ro ANGOS kardi , natonesti
+                bad miyay migi khodam hmaro mizanam< left bede
+            </p>
+            <button>ARASH</button>
+        </div>
+    </div>
+</div>
+<hr>
+<hr>
 @include('components.footer')
--- resources/views/realHome.blade.php
+++ resources/views/realHome.blade.php
@@ -4,33 +4,7 @@
 {{-- @include('components.navbar', ['services' => $services]) --}}
 @include('components.navbar')
 
-<div class="container-fluid introTemplate">
-    <div class="container">
-        <div class="row p-3">
-            <div class="col-12 col-md 6">
-                <section>
-                    <h2>Arash</h2>
-                    <h5 class="mt-4 mb-4">Arash Lorem ipsum Lorem ipsum dolor sit amet. corrupti. Temporibus nihil
-                        molestiae ipsum aliquid alias totam facere.</h5>
-                    <button>SALAM</button>
-                    <div>
-                        <div class="m-2">😘<div>Arash</div>
-                        </div>
-                        <div>
-                            <div>😘</div>Seifi
-                        </div>
-                    </div>
-                </section>
-            </div>
-            <div class="col-12 col-md 6 introTemplateImageContainer">
-                <div>
 
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@include('components.sliderLoop')
 <div class="container-fluid workWithUsHomeContainer">
     <div class="container">
         <div class="row"></div>
@@ -63,7 +37,38 @@
         </div>
     </div>
 </div>
+<div class="container-fluid introTemplate">
+    <div class="container">
+        <div class="row p-3">
+            <div class="col-12 col-md 6">
+                <section>
+                    <h2>Arash Salan miresone</h2>
+                    <h5 class="mt-4 mb-4">Arash Lorem ipsum Lorem ipsum dolor sit amet. corrupti. Temporibus nihil
+                        molestiae ipsum aliquid alias totam facere.
+                        Temporibus nihil
+                        molestiae ipsum aliquid alias totam facere.
+                        Temporibus nihil
+                        molestiae ipsum aliquid alias totam facere.
+                    </h5>
+                    <button>SALAM</button>
+                    <div>
+                        <div class="m-2">😘<div>Arash</div>
+                        </div>
+                        <div>
+                            <div>😘</div>Seifi
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="col-12 col-md 6 introTemplateImageContainer">
+                <div>
 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@include('components.sliderLoop')
 
 @include('components.faq')
 <br>
